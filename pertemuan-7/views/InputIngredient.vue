e11e8887a7b66326fad791e6f170410f500d236e
riyandrmd <ryanda.rama10@gmail.com>
2024-07-06 11:08:40 +0700

    bikin inputan

--- pertemuan-7/views/InputIngredient.vue
+++ pertemuan-7/views/InputIngredient.vue
@@ -1,3 +1,28 @@
+<script setup>
+import { reactive, ref } from "vue"
+</script>
+
 <template>
-    <h2>Ini Halaman Input Tagihan</h2>
+  <h1>List Ingredient</h1>
+  <label for="costumer">
+    Costumer
+    <input type="text" id="costumer" /> <br />
+  </label>
+  <span>
+    <label for="ingredient">
+      Ingredient :
+      <input type="text" id="ingredient" />
+    </label>
+    <label for="amount">
+      Amount :
+      <input type="number" id="amount" />
+    </label>
+    <label for="price">
+      Price :
+      <input type="number" id="price" />
+    </label>
+    <button type="submit">Submit</button>
+  </span>
+  <table></table>
 </template>
+
