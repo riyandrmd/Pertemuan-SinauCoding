5bee36dea93a05dcbbede56d547f02b8ef5b7e2e
riyandrmd <ryanda.rama10@gmail.com>
2024-07-06 16:36:19 +0700

    bikin tampilan tagihan

--- pertemuan-7/views/CetakTagihan.vue
+++ pertemuan-7/views/CetakTagihan.vue
@@ -1,3 +1,32 @@
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
+const router = useRouter();
+const totalAmount = route.query.totalAmount;
+const totalPrice = route.query.totalPrice;
+const namaCustomer = route.query.customer;
+
+console.log(route.query.totalPrice);
+</script>
+
 <template>
-    <h2>Ini Halaman Cetak Tagihan</h2>
+  <div>
+    <h1>Payment</h1>
+    <p>
+      Total pembayaran yang harus dibayarkan {{ namaCustomer }} dari
+      {{ totalAmount }} items sebesar <br />
+      Rp. {{ totalPrice }}
+    </p>
+    <button type="button" @click="router.back()">Back</button>
+  </div>
 </template>
+
+<style scoped>
+div {
+  border: 1px solid #dddddd;
+  border-radius: 5px;
+  padding: 10px;
+}
+</style>
