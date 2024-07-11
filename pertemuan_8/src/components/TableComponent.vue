b38de1d2254e1c1a5951b7de778fffccf5ebbdf4
riyandrmd <ryanda.rama10@gmail.com>
2024-07-11 16:02:48 +0700

    bikin table component

---
+++ pertemuan_8/src/components/TableComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  dataTable: {
+    type: Object,
+    default: {},
+  },
+});
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="(data, index) in dataTable.header" :key="index">
+          {{ data }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="data in dataTable.data" :key="data.id">
+        <td>{{ data.id }}</td>
+        <td>{{ data.nama }}</td>
+        <td>{{ data.position }}</td>
+        <td>{{ data.no_telp }}</td>
+        <td>{{ data.email }}</td>
+        <td><Button type="Submit">Submit</Button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ pertemuan_8/src/views/TableView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { reactive } from "vue";
+import TableComponent from "../components/TableComponent.vue";
+import Header from "../json/Header.json";
+import User from "../json/User.json";
+
+const tableData = reactive({
+  header: Header,
+  data: User,
+});
+
+</script>
+
+<template>
+  <div>
+    <h3>Table View</h3>
+    <TableComponent :dataTable="tableData" />
+  </div>
+</template>
